69949398b76364782ee045d6f61e23bb2d4c3c72
7118-eth <7118.eth@proton.me>
2024-09-22 12:47:37 +0400

    Add event emissions for assigning plant points and lifetime
    Implemented `WareHousePlantPointsAssigned` and `WareHouseLifetimeAssigned` events to track changes. Integrated `LibLand` functions to decrease accumulated points and lifetime before assigning new values.

--- src/libs/LibWareHouse.sol
+++ src/libs/LibWareHouse.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
+import { LibLand } from "../libs/LibLand.sol";
 import "./LibWareHouseStorage.sol";
 import "../shared/Structs.sol";
 import {ILandToPlant} from "../shared/ILandToPlant.sol";
@@ -9,12 +10,19 @@ library LibWareHouse {
 
     ILandToPlant internal constant landToPlant = ILandToPlant(0x1723a3F01895c207954d09F633a819c210d758c4);
 
-    function landToPlantAssignPlantPoints(uint256 _nftId, uint256 _addedPoints) internal returns (uint256 _newPlantPoints) {
-        return landToPlant.landToPlantAssignPlantPoints(_nftId, _addedPoints);
+    event WareHousePlantPointsAssigned(uint256 indexed landId, uint256 indexed plantId, uint256 addedPoints, uint256 newPlantPoints);
+    event WareHouseLifetimeAssigned(uint256 indexed landId, uint256 indexed plantId, uint256 lifetime, uint256 newLifetime);
+
+    function landToPlantAssignPlantPoints(uint256 _landId, uint256 _plantId, uint256 _addedPoints) internal returns (uint256 _newPlantPoints) {
+        LibLand._decreaseAccumulatedPlantPoints(_landId, _addedPoints);
+
+        _newPlantPoints = landToPlant.landToPlantAssignPlantPoints(_plantId, _addedPoints);
     }
 
-    function landToPlantAssignLifeTime(uint256 _nftId, uint256 _lifetime) internal returns (uint256 _newLifetime){
-        return landToPlant.landToPlantAssignLifeTime(_nftId, _lifetime);
+    function landToPlantAssignLifeTime(uint256 _landId, uint256 _plantId, uint256 _lifetime) internal returns (uint256 _newLifetime){
+        LibLand._decreaseAccumulatedPlantLifetime(_landId, _lifetime);
+
+        _newLifetime = landToPlant.landToPlantAssignLifeTime(_plantId, _lifetime);
     }
 
 
