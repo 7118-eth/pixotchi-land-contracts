63188968354c2eae00251a99ba1309c82ee10317
7118-eth <7118.eth@proton.me>
2024-10-02 19:10:32 +0200

    Update mainnet addresses in `LibConstants.sol`
    Replaced placeholder mainnet addresses for seed, leaf, and marketplace exchange with actual addresses. This ensures accurate and functional transactions in the mainnet environment.

--- src/libs/LibConstants.sol
+++ src/libs/LibConstants.sol
@@ -14,19 +14,19 @@ library LibConstants {
     // Constants for receive addresses
     address internal constant TESTNET_SEED_RECEIVE_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4);
     address internal constant TESTNET_LEAF_RECEIVE_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4);
-    address internal constant MAINNET_SEED_RECEIVE_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4); //TODO: Change this
-    address internal constant MAINNET_LEAF_RECEIVE_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4); //TODO: Change this
+    address internal constant MAINNET_SEED_RECEIVE_ADDRESS = address(0x2673b40F0ff6428BBEA795782603E823DF9E1c57); //TODO: Change this
+    address internal constant MAINNET_LEAF_RECEIVE_ADDRESS = address(0x2673b40F0ff6428BBEA795782603E823DF9E1c57); //TODO: Change this
 
     // Constants for receive addresses
     address internal constant TESTNET_SEED_SEND_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4);
     address internal constant TESTNET_LEAF_SEND_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4);
-    address internal constant MAINNET_SEED_SEND_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4); //TODO: Change this
-    address internal constant MAINNET_LEAF_SEND_ADDRESS = address(0x1b6742E960d77F416D1b14F1f837523103CAccB4); //TODO: Change this
+    address internal constant MAINNET_SEED_SEND_ADDRESS = address(0xd528071FB9dC9715ea8da44e2c4433EAc017d1DB); //TODO: Change this
+    address internal constant MAINNET_LEAF_SEND_ADDRESS = address(0xd528071FB9dC9715ea8da44e2c4433EAc017d1DB); //TODO: Change this
 
     // Constants for marketplace exchange addresses
-    address internal constant TESTNET_MARKETPLACE_EXCHANGE_ADDRESS = address(0x0000000000000000000000000000000000000000); // TODO: Update with actual address
+    address internal constant TESTNET_MARKETPLACE_EXCHANGE_ADDRESS = address(0xB9447202432ae9a4511cC33a14453A5A4968e0FC); // TODO: Update with actual address
     //address internal constant TESTNET_MARKETPLACE_LEAF_EXCHANGE_ADDRESS = address(0x0000000000000000000000000000000000000000); // TODO: Update with actual address
-    address internal constant MAINNET_MARKETPLACE_EXCHANGE_ADDRESS = address(0x0000000000000000000000000000000000000000); // TODO: Update with actual address
+    address internal constant MAINNET_MARKETPLACE_EXCHANGE_ADDRESS = address(0xB9447202432ae9a4511cC33a14453A5A4968e0FC); // TODO: Update with actual address
     //address internal constant MAINNET_MARKETPLACE_LEAF_EXCHANGE_ADDRESS = address(0x0000000000000000000000000000000000000000); // TODO: Update with actual address
 
 
